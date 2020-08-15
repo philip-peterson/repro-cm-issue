634d5d03d982e90fa8136b6e45e2c73e863267dc
Philip Peterson <philip.c.peterson@gmail.com>
2020-08-15 15:45:47 -0700

    Initial commit

---
+++ target/debug/deps/s1-8104729afa38365e.d
@@ -0,0 +1,5 @@
+/Users/crispico/Code/repro_cm_issue/target/debug/deps/s1-8104729afa38365e.rmeta: s1/src/main.rs
+
+/Users/crispico/Code/repro_cm_issue/target/debug/deps/s1-8104729afa38365e.d: s1/src/main.rs
+
+s1/src/main.rs:
---
+++ target/debug/deps/s2-4b69739114e92c9a.d
@@ -0,0 +1,5 @@
+/Users/crispico/Code/repro_cm_issue/target/debug/deps/s2-4b69739114e92c9a.rmeta: s2/src/main.rs
+
+/Users/crispico/Code/repro_cm_issue/target/debug/deps/s2-4b69739114e92c9a.d: s2/src/main.rs
+
+s2/src/main.rs:
